9bc2332385c5a3359ee41d043ee3a90142b3eaa0
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2024-04-06 15:37:57 +0530

    Update schedule.tf

--- schedule.tf
+++ schedule.tf
@@ -1,3 +1,7 @@
+data "google_project" "this_project" {
+  project_id = var.project_id
+}
+
 resource "google_compute_resource_policy" "uptime_schedule" {
   name        = "uptime-schedule"
   description = "Keep instances shut down during nighttime to save money"
@@ -10,4 +14,20 @@ resource "google_compute_resource_policy" "uptime_schedule" {
     }
     time_zone = var.uptime_schedule["time_zone"]
   }
+  depends_on = [google_compute_instance.demo]
+}
+
+resource "google_project_iam_custom_role" "start_stop" {
+  role_id     = "instanceScheduler"
+  title       = "Instance Scheduler"
+  description = "Adds the missing permissions that the Compute Engine System service account needs to be able to start/stop instances"
+  permissions = ["compute.instances.start", "compute.instances.stop"]
+  depends_on  = [google_compute_resource_policy.uptime_schedule]
+}
+
+resource "google_project_iam_member" "member" {
+  project    = var.project_id
+  role       = google_project_iam_custom_role.start_stop.name
+  member     = "serviceAccount:service-${data.google_project.this_project.number}@compute-system.iam.gserviceaccount.com"
+  depends_on = [google_compute_resource_policy.uptime_schedule]
 }
